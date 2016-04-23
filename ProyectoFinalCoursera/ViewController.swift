6480499132f19341755e2cf645b681d7176c38a7
Josman Pérez Expósito <josman.perez@udc.es>
2016-04-23 12:58:10 +0200

    Initial Commit

---
+++ ProyectoFinalCoursera/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ProyectoFinalCoursera
+//
+//  Created by Josman Pérez Expósito on 23/04/16.
+//  Copyright © 2016 personal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
